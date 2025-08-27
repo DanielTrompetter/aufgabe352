3fa3064791d3b876eeb3a362083f55fed44ad134
Daniel Trompetter <danieltrompetter@gmail.com>
2025-08-27 11:22:44 +0200

    added bonus 2

--- bin/aufgabe352.dart
+++ bin/aufgabe352.dart
@@ -10,8 +10,11 @@ void main(List<String> arguments)
   triplePrint('ist');
   triplePrint('langweilig');
 
-  // bonus
+  // bonus 1
   convertMinutesToTime(1337);
+
+  // bonus 2
+  analyzeGrades([3,2,1,5,6]);
 }
 
 // aufgabe 1
@@ -29,10 +32,39 @@ void triplePrint(String bla)
   }
 }
 
-// bonus
+// bonus 1
 void convertMinutesToTime(int minutes)
 {
   int hours = minutes ~/60;
   int remainingMinutes = minutes % 60; // kann man auch ersetzen mit 'int remainingMinutes = minutes - hours *  60;'
   print("$minutes Minuten sind $hours ${hours > 1 ? 'Stunden' : 'Stunde'} und $remainingMinutes Minuten");
+}
+
+// bonus 2
+void printGrades(List<int> grades) {
+  // Gibt alle Noten nacheinander aus
+  // z.B.: Note 1: 2, Note 2: 1, Note 3: 3
+  for(int counter = 0; counter <grades.length;counter++)  
+  {
+    print("Note ${counter+1}: ${grades[counter]}");
+  }
+}
+
+void calculateAndPrintAverage(List<int> grades) {
+  // Berechnet den Durchschnitt und gibt aus:
+  // "Der Durchschnitt ist: 2.0"
+  double average = 0.0;
+  for(int grade in grades)
+  {
+    average += grade;
+  }
+  average /= grades.length;
+  print("Der Durchschnitt ist: ${average.toStringAsFixed(2)}");
+}
+
+// Hauptfunktion:
+void analyzeGrades(List<int> grades) {
+  // Ruft beide Funktionen nacheinander auf
+  printGrades(grades);
+  calculateAndPrintAverage(grades);
 }
