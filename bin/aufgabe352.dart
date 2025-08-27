0fbb7eeb57e82c059edb8a5a032c488b2418d79c
Daniel Trompetter <danieltrompetter@gmail.com>
2025-08-27 13:26:37 +0200

    added bonus 3

--- bin/aufgabe352.dart
+++ bin/aufgabe352.dart
@@ -15,6 +15,9 @@ void main(List<String> arguments)
 
   // bonus 2
   analyzeGrades([3,2,1,5,6]);
+
+  // bonus 3
+  analyzeNumbers([-13,2,31,31,-67]);
 }
 
 // aufgabe 1
@@ -62,9 +65,73 @@ void calculateAndPrintAverage(List<int> grades) {
   print("Der Durchschnitt ist: ${average.toStringAsFixed(2)}");
 }
 
-// Hauptfunktion:
 void analyzeGrades(List<int> grades) {
   // Ruft beide Funktionen nacheinander auf
   printGrades(grades);
   calculateAndPrintAverage(grades);
+}
+
+// bonus 3
+void findAndPrintExtreme(List<int> numbers) {
+  // Findet und gibt kleinste und größte Zahl aus
+  // Zeigt auch deren Position in der Liste
+  int smallest = numbers.first;
+  int smallPos = 0;
+  int biggest = numbers.last;
+  int bigPos = 0;
+  for(int i = 0;i<numbers.length;i++)
+  {
+    int num = numbers[i];
+    if(smallest>num)
+    {
+      smallest = num;
+      smallPos = i+1;
+    }
+    if(biggest<num)
+    {
+      biggest = num;
+      bigPos = i+1;
+    }
+  }
+  print("Grösste Zahl ist $biggest an Positon $bigPos und kleinste Zahl ist $smallest an Position $smallPos");
+}
+
+void printNumberTypes(List<int> numbers) {
+  // Zählt und gibt aus:
+  // - Wie viele gerade/ungerade Zahlen
+  // - Wie viele positive/negative Zahlen
+  int ungerade = 0;
+  int gerade = 0;
+  int positiv = 0;
+  int negativ = 0;
+  for(int num in numbers)
+  {
+    ((num % 2) == 0)?gerade++:ungerade++;
+    (num >= 0)?positiv++:negativ++;
+  }
+  print("Das Feld hat $gerade gerade Zahlen und $ungerade ungerade Zahlen");
+  print("Das Feld hat $positiv positive Zahlen und $negativ negative Zahlen");
+}
+
+void printDistribution(List<int> numbers) {
+  // Gibt eine einfache Häufigkeitsverteilung aus
+  // z.B. wie oft kommt jede Zahl vor
+  Map<int, int> zaehlung = {};
+  for (var number in numbers) 
+  {
+    zaehlung[number] = (zaehlung[number] ?? 0) + 1;
+  }
+  for(int key in zaehlung.keys)
+  {
+    print("Die $key kommt ${zaehlung[key]}x vor.");
+  }
+}
+
+void analyzeNumbers(List<int> numbers) {
+  // Ruft alle Funktionen der Reihe nach auf
+  // Gibt eine übersichtliche Gesamtanalyse
+  print("Analysiere $numbers");
+  findAndPrintExtreme(numbers);
+  printNumberTypes(numbers);
+  printDistribution(numbers);
 }
